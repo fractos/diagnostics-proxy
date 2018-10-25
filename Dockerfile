6540a628bf199fec4ffc2c0569e15dc02ce76281
Adam Christie <fractos@gmail.com>
2018-10-25 10:42:06 +0000

    fixing environment.

--- Dockerfile
+++ Dockerfile
@@ -26,6 +26,10 @@ RUN mkdir -p /var/log/nginx && \
     ln -sf /dev/stdout /var/log/nginx/access.log && \
     ln -sf /dev/stdout /var/log/nginx/error.log
 
+ENV LISTEN_PORT 80
+ENV PROXY_TARGET_HOST 127.0.0.1
+ENV PROXY_TARGET_PORT 5000
+
 COPY run.sh /
 RUN chmod +x /run.sh
 
--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 # substitute environment variables for PROXY_TARGET_HOST, PROXY_TARGET_PORT and LISTEN_PORT
 echo Running environment substitution
+# workaround for envsubst wreaking havoc on nginx variables:
+export DOLLAR='$'
 envsubst < /etc/nginx/conf.d/mysite.template > /etc/nginx/nginx.conf
 
 echo Starting Nginx
